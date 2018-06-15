a184970d5a58fb6d2832224db22bb4a1b745c450
Lucaciu Mircea <lucaciumircea5@gmail.com>
2018-06-15 08:22:27 +0300

    refactor

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 from flask import Flask, json, request
 from sklearn.neural_network import MLPClassifier
 import pandas as pd
-
+from sklearn.model_selection import cross_val_predict
 # read from file
 attributes = []
 with open('dataset.txt') as f:
@@ -12,9 +12,9 @@ with open('dataset.txt') as f:
 with open('classes.txt') as f:
     classes = f.readlines()
 classes = [x.strip() for x in classes]
-# Attributes : cold, sunny , rainy , city break ,vacantion, economic, pool, hotel
+# Attributes : cold, sunny , rainy , city break ,vacantion, economic, pool, expensive
 
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(15,), random_state=1)
 h = clf.fit(attributes, classes)
 
 app = Flask(__name__)
@@ -29,13 +29,13 @@ def stereotype():
     vacantion = float(request.args.get('vacantion'))
     economic = float(request.args.get('economic'))
     pool = float(request.args.get('pool'))
-    hotel = float(request.args.get('hotel'))
-
-    user_type = clf.predict_proba([[cold, sunny, rainy, city_break, vacantion, economic, pool, hotel]])
-    prepare_response = pd.DataFrame(user_type, columns=clf.classes_)
-    res = prepare_response.to_json()
+    expensive = float(request.args.get('expensive'))
+    ceva = [coef.shape for coef in clf.coefs_]
+    user_type = clf.predict([[cold, sunny, rainy, city_break, vacantion, economic, pool, expensive]])
+    # prepare_response = pd.DataFrame(user_type, columns=clf.classes_)
+    # res = prepare_response.to_json()
     response = app.response_class(
-        response=json.dumps(res),
+        response=json.dumps(user_type[0]),
         status=200,
         mimetype='application/json'
     )
